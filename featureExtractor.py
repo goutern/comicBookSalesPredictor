fd9defd2d78aff16588874da273775faf04d6af7
Nicholas Goutermout <goutern@sunypoly.edu>
2020-05-01 17:14:29 -0400

    + restructured code to make it easier to tune

--- featureExtractor.py
+++ featureExtractor.py
@@ -46,201 +46,29 @@ from hypopt import GridSearch
 from sklearn.neural_network import MLPClassifier
 
 
+
+epochs = 25
+batch_size = 32
+image_count = 100
+test_run = False
+generate_data = False
+load_image_data = False
+load_features = True
+load_features_flat = False
+show_dist_graphs = True
+
+limit_memory = False
+
+remove_outliers = True
+threshold = 1
+
+
 def swish(x, beta = 1):
     return (x * sigmoid(beta * x))
 
 get_custom_objects().update({'swish': Activation(swish)})
 
 
-class EarlyStoppingByLossVal(Callback):
-    def __init__(self, monitor='val_loss', value=0.00001, verbose=0):
-        super(Callback, self).__init__()
-        self.monitor = monitor
-        self.value = value
-        self.verbose = verbose
-
-    def on_epoch_end(self, epoch, logs={}):
-        current = logs.get(self.monitor)
-        if current is None:
-            warnings.warn("Early stopping requires %s available!" % self.monitor, RuntimeWarning)
-
-        if current < self.value:
-            if self.verbose > 0:
-                print("Epoch %05d: early stopping THR" % epoch)
-            self.model.stop_training = True
-
-#get out "trial development" data
-
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# if gpus:
-#   try:
-#     tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024
-#                                                                                                                         )])
-#   except RuntimeError as e:
-#     print(e)
-
-data = np.load('cleanData.npy',allow_pickle=True)
-# sortedData = data[np.argsort(data[:,6])]
-epochs = 25
-batch_size = 128
-image_count = 100
-test_run = False
-load_train_data = True
-show_dist_graphs = False
-threshold = 3
-seed = 1
-# data = data[:10000,:]
-# x is your dataset
-# x = np.random.rand(100, 5)
-# indices = np.random.permutation(data.shape[0])[:image_count]
-
-#
-#
-# print(y_train[0:10])
-#
-#
-# print("Training data available in 3000 classes")
-# print([train_y.count(i) for i in range(0, 11)])
-
-
-# # Show our data distibution
-# y_pos = np.arange(num_classes)
-# counts = [train_y.count(i) for i in range(0, num_classes)]
-#
-# plt.barh(y_pos, counts, align='center', alpha=0.5)
-# # plt.yticks(y_pos)
-# plt.xlabel('Counts')
-# plt.title('Train Data Class Distribution')
-# plt.show()
-
-# Show our data distibution
-
-z = np.abs(stats.zscore(data[:,6].astype(int)))
-
-print(np.where(z > threshold))
-print(data.shape)
-
-data = data[(z < threshold)]
-print(data.shape)
-
-
-# Q1 = data[:,6].quantile(0.25)
-# Q3 = data[:,6].quantile(0.75)
-# IQR = Q3 - Q1
-# print(IQR)
-#
-# data = data[~((data < (Q1 - 1.5 * IQR)) |(data > (Q3 + 1.5 * IQR))).any(axis=1)]
-# print(data.shape)
-
-if test_run:
-    indices = np.random.permutation(data.shape[0])[:image_count]
-else:
-    indices = np.random.permutation(data.shape[0])
-index = int(0.6 * len(indices))
-percent = int(0.2 * len(indices))
-
-training_idx, test_idx, val_idx = indices[:index], indices[index:index+percent], indices[index+percent:]
-training, test, val = data[training_idx,:], data[test_idx,:], data[val_idx,:]
-
-model = VGG16(weights='imagenet', include_top=False)
-model.summary()
-# data = np.load('cleanData2019L.npy',allow_pickle=True)
-# data = np.delete(data, 0,0)
-# valData = np.load('cleanData2018VAL.npy', allow_pickle=True)
-# valData = np.delete(data, 0,0);
-# testData = np.load('cleanData2017test.npy', allow_pickle=True)
-# testData =  np.delete(data, 0,0)
-# featureData = np.zeros(shape=(1,10))
-train_y = []
-val_y = []
-test_y = []
-# num_classes = 1150
-
-#get the sales numbers
-for comic in training:
-    train_y.append(int(comic[6]))
-
-for comic in val:
-    val_y.append(int(comic[6]))
-
-for comic in test:
-    test_y.append(int(comic[6]))
-
-# print(train_y[0:10])
-
-#incode our sales numbers categories using one hot encoding
-# y_train = np_utils.to_categorical(train_y, num_classes)
-# y_val = np_utils.to_categorical(val_y, num_classes)
-# y_test = np_utils.to_categorical(test_y, num_classes)
-y_train = train_y
-y_val = val_y
-y_test = test_y
-
-
-
-
-if show_dist_graphs:
-    yPlot = data[:,6]
-    y_pos = np.sort(yPlot.astype(int))
-    sns.set(color_codes=True)
-    sns.distplot(y_pos)
-    plt.show()
-
-# sns.boxplot(y_pos)
-# plt.show()
-
-
-    yPlot = training[:,6]
-    y_pos = np.sort(yPlot.astype(int))
-    x_pos = np.arange(len(yPlot))
-
-    plt.scatter(x_pos, y_pos, alpha=0.5, color='red')
-    plt.show()
-
-
-
-    yPlot = test[:,6]
-    y_pos = np.sort(yPlot.astype(int))
-    x_pos = np.arange(len(yPlot))
-    plt.scatter(x_pos, y_pos, alpha=0.5, color='blue')
-    plt.show()
-
-    yPlot = val[:,6]
-    y_pos = np.sort(yPlot.astype(int))
-    x_pos = np.arange(len(yPlot))
-    plt.scatter(x_pos, y_pos, alpha=0.5,color='green')
-    plt.show()
-
-
-# counts = [data[:,:6]]
-# print(1)
-# plt.barh(y_pos,x_pos, align='center', alpha=0.5)
-# print(1)
-# plt.yticks(y_pos)
-# print(1)
-# plt.xlabel('Percent Total')
-# print(1)
-# plt.title('Train Data Class Distribution')
-# print(1)
-# plt.show()
-# print(1)
-
-
-
-# # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-# labels = np.arange(num_classes)
-# sizes = [train_y.count(i) for i in range(0, num_classes)]
-# # explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-#
-# fig1, ax1 = plt.subplots()
-# ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-#         shadow=True, startangle=90)
-# ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-#
-# plt.show()
-
-
-# Extract the features using the pretrained VGG
 def create_features(dataSource, pre_model):
     x_scratch = []
     for comic in dataSource:
@@ -262,136 +90,164 @@ def create_features(dataSource, pre_model):
 
 
 
-# train_x, train_features, train_features_flatten = create_features(data, model)
-# print(train_x.shape, train_features.shape, train_features_flatten.shape)
-# val_x, val_features, val_features_flatten = create_features(valData, model)
-# test_x, test_features, test_features_flatten = create_features(testData, model)
+class EarlyStoppingByLossVal(Callback):
+    def __init__(self, monitor='val_loss', value=0.00001, verbose=0):
+        super(Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+        self.verbose = verbose
 
-if load_train_data:
-    print("Loading Features")
-    train_features = np.load("train_features.npy")
-    val_features = np.load("val_features.npy")
-    test_features = np.load("test_features.npy")
-else:
-    print("Creating Features")
-    train_x, train_features, train_features_flatten = create_features(training, model)
-    print(train_x.shape, train_features.shape)
-    val_x, val_features,val_features_flatten = create_features(val, model)
-    print(val_x.shape, val_features.shape)
-    test_x, test_features,test_features_flatten = create_features(test, model)
-    print(test_x.shape, test_features.shape)
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        if current is None:
+            warnings.warn("Early stopping requires %s available!" % self.monitor, RuntimeWarning)
 
-    np.save("train_features.npy", train_features)
-    np.save("val_features.npy", val_features)
-    np.save("test_features.npy", test_features)
+        if current < self.value:
+            if self.verbose > 0:
+                print("Epoch %05d: early stopping THR" % epoch)
+            self.model.stop_training = True
 
-
-
-# nsamples, nx, ny = train_x.shape[:-1]
-# d2_train_dataset = train_x.reshape((nsamples,nx*ny))
-# regressor = LinearRegression()
-# regressor.fit(d2_train_dataset, y_train) #training the algorithm
-# val_x, val_features, val_features_flatten = create_features(val, model)
-# test_x, test_features, test_features_flatten = create_features(test, model)
-
-# Creating a checkpointer
-checkpointer = ModelCheckpoint(filepath='scratchmodel.best.hdf5',
-                               verbose=1, save_best_only=True)
-#
-# # Building up a Sequential model
-# model_scratch = Sequential()
-# model_scratch.add(Conv2D(32, (3, 3), activation='relu', input_shape=train_x.shape[1:]))
-# model_scratch.add(MaxPooling2D(pool_size=(2, 2)))
-#
-# model_scratch.add(Conv2D(64, (3, 3), activation='relu'))
-# model_scratch.add(MaxPooling2D(pool_size=(2, 2)))
-#
-# model_scratch.add(Conv2D(64, (3, 3), activation='relu'))
-# model_scratch.add(MaxPooling2D(pool_size=(2, 2)))
-#
-# model_scratch.add(Conv2D(128, (3, 3), activation='relu'))
-# model_scratch.add(MaxPooling2D(pool_size=(2, 2)))
-#
-#
-#
-# model_scratch.add(GlobalAveragePooling2D())
-# model_scratch.add(Dense(64, activation='relu'))
-# # model_scratch.add(Dropout(0.1))
-# model_scratch.add(Dense(64, activation='relu'))
-# model_scratch.add(Dropout(0.1))
-# model_scratch.add(Dense(1, activation='relu'))
-# model_scratch.summary()
-#
-# model_scratch.compile(loss='mean_squared_error', optimizer='adam',
-#                       metrics=['accuracy'])
-callbacks = [
-    EarlyStoppingByLossVal(monitor='val_loss', value=0.00001, verbose=1),
-    # EarlyStopping(monitor='val_loss', patience=2, verbose=0),
-    checkpointer]
-# Fitting the model on the train data and labels.
-# history = model_scratch.fit(train_x, y_train,
-#                             batch_size=32, epochs=epochs,
-#                             verbose=1, callbacks=callbacks,
-#                             validation_data=(val_x, y_val), shuffle=True)
-
-# model = Sequential()
-# model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
-# model.add(Dense(1, kernel_initializer='normal'))
 
 
 def plot_loss(history):
     fig = plt.figure(figsize=(10, 5))
-    # plt.subplot(1, 2, 1)
-    # plt.plot(history.history['accuracy'])
-    # plt.plot(history.history['val_accuracy'])
-    # plt.title('model accuracy')
-    # plt.ylabel('accuracy')
-    # plt.xlabel('epoch')
-    # plt.legend(['train', 'validation'], loc='upper left')
-
-    # plt.subplot(1, 2, 2)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper right')
+    plt.legend(['train', 'validation'], loc='upper right')
+    plt.show()
+
+if limit_memory:
+# Save memory just in case
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+        except RuntimeError as e:
+            print(e)
+
+
+#Load up the clean data
+clean_data = np.load('cleanData.npy',allow_pickle=True)
+data_y = []
+data = clean_data
+
+def create_y(data):
+    for comic in data:
+        data_y.append(int(comic[6]))
+    np.save("data_y.npy", data_y)
+
+
+if generate_data:
+    create_y(data)
+    model = VGG16(weights='imagenet', include_top=False)
+    model.summary()
+
+    print("Creating Features")
+    image_data, features, features_flatten = create_features(clean_data, model)
+
+    np.save("image_data.npy", image_data)
+    np.save("features.npy", features)
+    np.save("features_flatten.npy", features_flatten)
+
+else:
+    print("Loading Data Set")
+    if load_image_data:
+        print("Loading image data")
+        image_data = np.load("image_data.npy")
+    if load_features:
+        print("Loading feature data")
+        features = np.load("features.npy")
+    if load_features_flat:
+        print("Loading flattend feature data")
+        features_flatten = np.load("features_flatten.npy")
+    data_y = np.load("data_y.npy")
+
+
+
+if remove_outliers:
+    #Remove outliers
+    print("Removing Outliers")
+    z = np.abs(stats.zscore(data[:,6].astype(int)))
+
+    # print(np.where(z > threshold))
+    print(data.shape)
+    print(data_y.shape)
+
+    data = data[(z < threshold)]
+    data_y = data_y[(z < threshold)]
+
+    print("Outliers Removed")
+    print(data.shape)
+    print(data_y.shape)
+
+
+#used to reduce the image pool to run faster tests
+if test_run:
+    print("Test Run Activated Running on Reduced Data Set")
+    print("Image Count: " + str(image_count))
+    indices = np.random.permutation(data.shape[0])[:image_count]
+else:
+    indices = np.random.permutation(data.shape[0])
+index = int(0.6 * len(indices))
+percent = int(0.2 * len(indices))
+
+
+#train test validation split
+training_idx, test_idx, val_idx = indices[:index], indices[index:index+percent], indices[index+percent:]
+train, test, val = data[training_idx,:], data[test_idx,:], data[val_idx,:]
+
+if load_image_data:
+    train_image, test_image, val_image = image_data[training_idx,:], image_data[test_idx,:], image_data[val_idx,:]
+
+if load_features:
+    train_features, test_features, val_features = features[training_idx,:], features[test_idx,:], features[val_idx,:]
+
+if load_features_flat:
+    train_features_flatten, test_features_flatten, val_features_flatten = features_flatten[training_idx,:], features_flatten[test_idx,:], features_flatten[val_idx,:]
+
+train_y, test_y, val_y = data_y[training_idx], data_y[test_idx], data_y[val_idx]
+
+
+
+
+if show_dist_graphs:
+    yPlot = data_y
+    y_pos = np.sort(yPlot.astype(int))
+    sns.set(color_codes=True)
+    sns.distplot(y_pos)
+    plt.show()
+
+    yPlot = train_y
+    y_pos = np.sort(yPlot.astype(int))
+    x_pos = np.arange(len(yPlot))
+    plt.scatter(x_pos, y_pos, alpha=0.5, color='red')
+    plt.show()
+
+    yPlot = test_y
+    y_pos = np.sort(yPlot.astype(int))
+    x_pos = np.arange(len(yPlot))
+    plt.scatter(x_pos, y_pos, alpha=0.5, color='blue')
+    plt.show()
+
+    yPlot = val_y
+    y_pos = np.sort(yPlot.astype(int))
+    x_pos = np.arange(len(yPlot))
+    plt.scatter(x_pos, y_pos, alpha=0.5,color='green')
     plt.show()
 
 
-# plot_acc_loss(history)
-#
-# preds = np.argmax(model_scratch.predict(test_x), axis=1)
-# print("\nAccuracy on Test Data: ", accuracy_score(test_y, preds))
-# print("\nNumber of correctly identified imgaes: ",
-#       accuracy_score(test_y, preds, normalize=False),"\n")
-# # confusion_matrix(test_y, preds, labels=range(0,num_classes))
 
+# Creating a checkpointer
+checkpointer = ModelCheckpoint(filepath='scratchmodel.best.hdf5',
+                               verbose=1, save_best_only=True)
 
+callbacks = [
+    EarlyStoppingByLossVal(monitor='val_loss', value=0.00001, verbose=1),
+    checkpointer]
 
-# model = MLPClassifier()
-#
-#
-# model.fit(train_features, y_train)
-#
-#
-#
-#
-#
-# model_transfer = Sequential()
-# model_transfer.add(GlobalAveragePooling2D(input_shape=train_features.shape[1:]))
-# model_transfer.add(Dense(64, activation='relu'))
-# # model_scratch.add(Dropout(0.1))
-# model_transfer.add(Dense(64, activation='relu'))
-# # model_transfer.add(Dropout(0.1))
-# model_transfer.add(Dense(1, activation='relu'))
-# model_transfer.summary()
-#
-# model_transfer.compile(loss='mean_squared_error', optimizer='adam',
-#               metrics=['accuracy'])
-# history = model_transfer.fit(train_features, y_train, batch_size=32, epochs=epochs,
-#           validation_data=(val_features, y_val), callbacks=callbacks,
-#           verbose=1, shuffle=True)
 
 def baseline_model():
     model = Sequential()
@@ -413,21 +269,26 @@ estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=100, batch_size=100
 # results = cross_val_score(estimator, train_features, y_train, cv=kfold)
 # print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
 
-history = estimator.fit(train_features, y_train, batch_size=batch_size, epochs=epochs,
-          validation_data=(val_features, y_val), callbacks=callbacks,
+history = estimator.fit(train_features, train_y, batch_size=batch_size, epochs=epochs,
+          validation_data=(val_features, val_y), callbacks=callbacks,
           verbose=1, shuffle=True)
-prediction = estimator.predict(train_features)
+prediction = estimator.predict(test_features)
 
-train_error =  np.abs(y_train - prediction)
-mean_error = np.mean(train_error)
-min_error = np.min(train_error)
-max_error = np.max(train_error)
-std_error = np.std(train_error)
+test_error =  np.abs(test_y - prediction)
+mean_error = np.mean(test_error)
+min_error = np.min(test_error)
+max_error = np.max(test_error)
+std_error = np.std(test_error)
 
 plot_loss(history)
 
+plt.scatter(test_y, prediction)
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+plt.show()
 
-y_pos = np.sort(train_error.astype(int))
+
+y_pos = np.sort(test_error.astype(int))
 x_pos = np.arange(len(y_pos))
 plt.scatter(x_pos, y_pos, alpha=0.5, color='red')
 plt.show()
@@ -441,31 +302,3 @@ print("Mean Error:" + str(mean_error))
 print("Min Error:" + str(min_error))
 print("Max Error:" + str(max_error))
 print("Std Error:" + str(std_error))
-# preds = np.argmax(prediction.predict(test_features_flatten), axis=1)
-# print("\nAccuracy on Test Data: ", accuracy_score(test_y, prediction))
-# print("\nNumber of correctly identified imgaes: ",
-#       accuracy_score(test_y, prediction, normalize=False),"\n")
-# confusion_matrix(test_y, preds, labels=range(0,num_classes))
-
-#
-# print("1")
-# param_grid = [{'C': [0.1, 1, 10], 'solver': ['newton-cg', 'lbfgs']}]
-# print("2")
-# # wait()
-# # # Grid-search all parameter combinations using a validation set.
-# opt = GridSearch(model=LogisticRegression(class_weight='balanced', multi_class="auto",
-#                                           max_iter=4000, random_state=1), param_grid=param_grid, num_threads=4, parallelize=False)
-# print("3")
-# opt.fit(train_features_flatten, train_y, val_features_flatten, val_y, scoring='accuracy')
-# print("4")
-# print(opt.get_best_params())
-# print("5")
-# opt.score(test_features_flatten, test_y)
-# print("6")
-# preds = opt.predict(test_features_flatten)
-# print("7")
-# print("\nAccuracy on Test Data: ", accuracy_score(test_y, preds))
-# print("8")
-# print("\nNumber of correctly identified imgaes: ",
-# accuracy_score(test_y, preds, normalize=False),"\n")
-# confusion_matrix(test_y, preds, labels=range(0,num_classes))
